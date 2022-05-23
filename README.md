a79296a43766dad05136b0cea0c41bc51e09dc4c
xenxynon <flynryder427@gmail.com>
2022-05-23 15:02:57 +0000

    Add extras for qssi-user 11 RKQ1.201004.002 V12.5.7.0.RJFMIXM release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.201004.002 V12.5.7.0.RJFMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: citrus
+- Brand: POCO
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.201004.002
+- Incremental: V12.5.7.0.RJFMIXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: POCO/citrus_global/citrus:11/RKQ1.201004.002/V12.5.7.0.RJFMIXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.201004.002-V12.5.7.0.RJFMIXM-release-keys
+- Repo: poco_citrus_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
+cat bootimg/15_dtbdump_U$9gO#__'l|_?.dtb.* 2>/dev/null >> bootimg/15_dtbdump_U$9gO#__'l|_?.dtb
+rm -f bootimg/15_dtbdump_U$9gO#__'l|_?.dtb.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk
+rm -f system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null
+cat system/system/app/Notes/Notes.apk.* 2>/dev/null >> system/system/app/Notes/Notes.apk
+rm -f system/system/app/Notes/Notes.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiGallery/MiuiGallery.apk.* 2>/dev/null >> system/system/priv-app/MiuiGallery/MiuiGallery.apk
+rm -f system/system/priv-app/MiuiGallery/MiuiGallery.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.zhiliaoapp.musically_42/partner-com.zhiliaoapp.musically_42.apk.* 2>/dev/null >> cust/app/customized/partner-com.zhiliaoapp.musically_42/partner-com.zhiliaoapp.musically_42.apk
+rm -f cust/app/customized/partner-com.zhiliaoapp.musically_42/partner-com.zhiliaoapp.musically_42.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk.* 2>/dev/null >> cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk
+rm -f cust/app/customized/partner-com.kwai.kuaishou.video.live_155/partner-com.kwai.kuaishou.video.live_155.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk.* 2>/dev/null >> cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk
+rm -f cust/app/customized/partner-com.agoda.mobile.consumer_110/partner-com.agoda.mobile.consumer_110.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.zhiliaoapp.musically_100/partner-com.zhiliaoapp.musically_100.apk.* 2>/dev/null >> cust/app/customized/partner-com.zhiliaoapp.musically_100/partner-com.zhiliaoapp.musically_100.apk
+rm -f cust/app/customized/partner-com.zhiliaoapp.musically_100/partner-com.zhiliaoapp.musically_100.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk.* 2>/dev/null >> cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk
+rm -f cust/app/customized/partner-com.amazon.mp3_123/partner-com.amazon.mp3_123.apk.* 2>/dev/null
